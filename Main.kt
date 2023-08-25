7831487151ff64e3da505ad0f53f31eb4707e1e9
AlbertKloster <albert.kloster@gmail.com>
2023-08-25 07:13:42 -0400

    stage2

--- Main.kt
+++ Main.kt
@@ -1,31 +1,74 @@
 package seamcarving
 
 import java.awt.Color
-import java.awt.image.BufferedImage
 import java.io.File
+import java.io.IOException
 import javax.imageio.ImageIO
 
+fun main(args: Array<String>) {
 
-fun main() {
-    println("Enter rectangle width:")
-    val width = readln().toInt()
+    val inputFile = getInputFile(args)
+    val outputFile = getOutputFile(args)
 
-    println("Enter rectangle height:")
-    val height = readln().toInt()
+    if (inputFile == null || outputFile == null) {
+        println("Wrong arguments")
+        return
+    }
 
-    println("Enter output image name:")
-    val imageName = readln()
+    val bufferedImage = try {
+        ImageIO.read(File(inputFile))
+    } catch (e: IOException) {
+        println("Input file $inputFile not found")
+        return
+    }
 
-    val bufferedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
-    val graphics = bufferedImage.createGraphics()
+    for (x in 0 until bufferedImage.width) {
+        for (y in 0 until bufferedImage.height) {
+            val color = Color(bufferedImage.getRGB(x, y))
+            val r = 255 - color.red
+            val g = 255 - color.green
+            val b = 255 - color.blue
+            bufferedImage.setRGB(x, y, Color(r, g, b).rgb)
+        }
+    }
 
-    graphics.paint = Color.BLACK
-    graphics.fillRect(0, 0, width, height)
+    ImageIO.write(bufferedImage, "png", File(outputFile))
 
-    graphics.color = Color.RED
-    graphics.drawLine(0, 0, width - 1, height - 1)
-    graphics.drawLine(0, height - 1, width - 1, 0)
 
-    ImageIO.write(bufferedImage, "png", File(imageName))
 
+//    println("Enter rectangle width:")
+//    val width = readln().toInt()
+//
+//    println("Enter rectangle height:")
+//    val height = readln().toInt()
+//
+//    println("Enter output image name:")
+//    val imageName = readln()
+//
+//    val bufferedImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+//    val graphics = bufferedImage.createGraphics()
+//
+//    graphics.paint = Color.BLACK
+//    graphics.fillRect(0, 0, width, height)
+//
+//    graphics.color = Color.RED
+//    graphics.drawLine(0, 0, width - 1, height - 1)
+//    graphics.drawLine(0, height - 1, width - 1, 0)
+//
+//    ImageIO.write(bufferedImage, "png", File(imageName))
+
+}
+
+private fun getInputFile(args: Array<String>): String? {
+    val indexIn = args.indexOf("-in")
+    if (indexIn != -1 && args.size > indexIn + 1)
+        return args[indexIn + 1]
+    return null
+}
+
+private fun getOutputFile(args: Array<String>): String? {
+    val indexIn = args.indexOf("-out")
+    if (indexIn != -1 && args.size > indexIn + 1)
+        return args[indexIn + 1]
+    return null
 }
--- README.md
+++ README.md
@@ -1,24 +1,21 @@
-# Stage 1/6: Create an image
+# Stage 2/6: Negative photo
 ## Description
-At this stage, you'll learn some basic image manipulation.
+Before digital photography came around, people used to work with negative images on film. Let’s get nostalgic and emulate a warm analog negative film image!
+
+To create a negative image, you should invert all color components for every pixel. Inverted color for `(r, g, b)` is `(255 - r, 255 - g, 255 - b)`.
 
 ## Objective
-Your task is to draw a black rectangle of a given size, draw two red lines crossing it and save the image. Your program should read the rectangle's width and height, as well as the output file name from the input. We will use only `.png` file format for this project.
-
-We recommend to use `java.awt.image.BufferedImage` class to handle and manipulate the image data and `javax.imageio.ImageIO` for reading and saving images from/to a file. You can check out some nice explanations and examples of <a href="https://www.tutorialspoint.com/java_dip/java_buffered_image.htm">BufferedImage</a> and <a href="https://www.tutorialspoint.com/How-to-convert-Byte-Array-to-Image-in-java">ImageIO</a> to be more confident in what you're doing. You can also look at official documentation on <a href="https://docs.oracle.com/javase/8/docs/api/java/awt/image/BufferedImage.html">BufferedImage</a> and <a href="https://docs.oracle.com/javase/8/docs/api/javax/imageio/ImageIO.html">ImageIO</a> classes.
-
-Tests use hash comparison of output files to test your solution. Using the proposed classes will help you avoid unintended difficulties.
+At this stage, you should add command-line parameters to specify the input and output files for your program. Use parameter `-in` for the input file path and parameter `-out` for the output file path. For simplicity, we will use only `.png` file format in this project. Your program should read the specified input image file, inverse colors and save the file to a given location.
 
 ## Example
 The greater-than symbol followed by a space (`> `) represents the user input. Note that it's not part of the input.
 ```
-Enter rectangle width:
-> 20
-Enter rectangle height:
-> 10
-Enter output image name:
-> out.png
+> java Main -in sky.png -out sky_negative.png
 ```
-`out.png` should look like this:
+For the following `sky.png`:
 
-![crossed red lines](image.png)
+![river side building construction](sky.png)
+
+`sky_negative.png` should look like this:
+
+![river side building construction negative](sky_negative.png)
